f61ff50c3eb0d3c8b348e7ce58153394582c5df7
Yelyzaveta Lubenets <el1111zavetaaaaa@gmail.com>
2022-10-31 14:42:39 +0200

    lecture 3,4 home task

--- lecture4/multipleDoubleSquareFunctions.js
+++ lecture4/multipleDoubleSquareFunctions.js
@@ -1,24 +1,18 @@
 const prompt = require("prompt-sync")();
 
-let inputNumbers = prompt("Please, enter 2 numbers,separated with commas (example: 2,4):");
+let inputNumber = prompt("Please, enter the number:");
 
 function multiply(num1,num2){
     return num1 * num2;
 }
 
-function double(num_func){
-    return num_func + num_func;
+function double(num){
+    return multiply(num, 2);
 }
 
-function square(num_func){
-    return Math.pow(num_func, 2);
+function square(num){
+    return multiply(num, num);
 }
 
-firstNumber = inputNumbers.split(',')[0]
-
-secondNumber = inputNumbers.split(',')[1]
-
-let multiply_result = multiply(firstNumber, secondNumber)
-
-console.log(`Double value: ${double(multiply_result)}.`)
-console.log(`Square: ${square(multiply_result)}.`)
+console.log(`Double value: ${double(inputNumber)}.`)
+console.log(`Square: ${square(inputNumber)}.`)
